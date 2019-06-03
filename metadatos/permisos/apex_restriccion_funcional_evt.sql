e9174027d4ca4524d03efcd742b5e74c754481c5
Andrea <Andrea@AndreaGran>
2019-06-03 11:04:28 -0300

    Restriccion de Dependencias

--- metadatos/permisos/apex_restriccion_funcional_evt.sql
+++ metadatos/permisos/apex_restriccion_funcional_evt.sql
@@ -25,6 +25,13 @@ INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, ite
 );
 INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
 	'formularios', --proyecto
+	'49', --restriccion_funcional
+	'3814', --item
+	'3546', --evento_id
+	'1'  --no_visible
+);
+INSERT INTO apex_restriccion_funcional_evt (proyecto, restriccion_funcional, item, evento_id, no_visible) VALUES (
+	'formularios', --proyecto
 	'50', --restriccion_funcional
 	'3814', --item
 	'3463', --evento_id
