5af463726bfd4616ad51102298a1ca57ccf92b64
Andrea <Andrea@LAPTOP-CK2T37FA>
2022-05-11 12:55:13 -0300

    No repite numero transferencia, que no considere los formularios anulados

--- php/datos/dt_modalidad_pago.php
+++ php/datos/dt_modalidad_pago.php
@@ -30,13 +30,18 @@ class dt_modalidad_pago extends toba_datos_tabla
    
     function no_repite_transferencia($nro_transf){//retorna array vacio sino se repite
         $resul=array();
-        $sql="select * from modalidad_pago where nro_transferencia=".$nro_transf;
+        $sql="select * from modalidad_pago, formulario f
+              where m.id_form=f.id_form 
+              and f.estado<>'N'
+              and nro_transferencia=".$nro_transf;
         $resul = toba::db('formularios')->consultar($sql);
         return $resul;
     }
     function no_repite_transferencia_modif($nro_transf,$id_mod){//retorna true sino se repite
         $resul=array();
-        $sql="select * from modalidad_pago where nro_transferencia=".$nro_transf." and id_mod<>".$id_mod;
+        $sql="select * from modalidad_pago , formulario f "
+                . " where m.id_form=f.id_form 
+                    and f.estado<>'N' and nro_transferencia=".$nro_transf." and id_mod<>".$id_mod;
         $resul = toba::db('formularios')->consultar($sql);
         return $resul;
     }
