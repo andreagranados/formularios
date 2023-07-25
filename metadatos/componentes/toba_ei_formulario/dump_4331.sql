2959a67b9ef2a82525268b477019cb6402bf546c
Andrea <Andrea@LAPTOP-CK2T37FA>
2023-07-25 12:26:31 -0300

    Tipo de Cuenta: MP (Mercado Pago) para Cuentas Bancarias

--- metadatos/componentes/toba_ei_formulario/dump_4331.sql
+++ metadatos/componentes/toba_ei_formulario/dump_4331.sql
@@ -340,7 +340,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	NULL, --carga_consulta_php
 	NULL, --carga_sql
 	'formularios', --carga_fuente
-	'CC,CA', --carga_lista
+	'CC,CA,MP', --carga_lista
 	NULL, --carga_col_clave
 	NULL, --carga_col_desc
 	NULL, --carga_maestros
